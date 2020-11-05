5eccf5ca9b0da09f00ffac3a3d91c66f4c2bc15e
Rafael Carbonell Lázaro <racarla96@gmail.com>
2020-11-05 12:58:53 +0100

    Example working!

---
+++ examples/readAndPrint/readAndPrint.ino
@@ -0,0 +1,26 @@
+#include <marvelmind.h>
+
+MarvelmindHedge hedge;
+
+unsigned long t1;
+const unsigned long period = 1000;
+long baudrate = 115200;
+
+void setup() {
+  hedge.begin(&Serial1, baudrate, &Serial, baudrate);
+  t1 = millis();
+}
+
+void loop() {
+  hedge.read();
+  if(millis() - t1 > period){
+//    hedge.printPositionFromMarvelmindHedge(true);
+//    hedge.printStationaryBeaconsPositionsFromMarvelmindHedge(true);
+//    hedge.printRawDistancesFromMarvelmindHedge(true);  
+//    hedge.printRawIMUFromMarvelmindHedge(true);
+    hedge.printFusionIMUFromMarvelmindHedge(true);    
+    hedge.printTelemetryFromMarvelmindHedge(true);
+//    hedge.printQualityFromMarvelmindHedge(true);  
+    t1 = millis(); 
+  }
+}
